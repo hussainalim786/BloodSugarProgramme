997be4afd960a0015d1b7620bf9a70fc520fb3fb
hussainalim786 <75867232+hussainalim786@users.noreply.github.com>
2020-12-12 15:10:38 +0500

    Add files via upload

---
+++ BloodSugarProgramme.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bloodsugarprogramme;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hussain Alim
+ */
+public class BloodSugarProgramme {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+              // Blood Sugar Chek up Programme!
+ 
+int i;
+String n;
+        System.out.println("Welcome to Blood Sugar Check up Programme!");
+        
+
+       Scanner input= new Scanner(System.in);
+       System.out.println("Enter the Patient Last Name");
+       n=input.next();
+       System.out.println(" Please Enter the Number!");
+       i=input.nextInt();
+       
+if(i==0)
+            System.out.println("Blood Sugar is Low!"); 
+else
+{
+if(i==1)
+    System.out.println("Blood Sugar is Low!"); 
+else
+{
+if(i==2)
+    System.out.println("Blood Sugar is Low!"); 
+else
+{
+if(i==3)
+    System.out.println("Blood Sugar is Low!"); 
+else
+{
+if(i==4)
+    System.out.println("Blood Sugar is Low!"); 
+else
+{
+if(i==5)
+    System.out.println("Perfect Blood Sugar!"); 
+  
+else
+    System.out.println("Blood Sugar is High!"); 
+
+}}}}}
+  }
+    }
+    
+    
+    
+
